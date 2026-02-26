d60b9b9bd522581582b298e78d08a2bb0d5dbddb
Rishab Raj <rr0948555@gmail.com>
2026-02-26 22:51:49 +0530

    Fibonacci

---
+++ Recursion/rec5.js
@@ -0,0 +1,10 @@
+// Fibonacci Series => f(n) = f(n-1) + f(n-2)
+// f(0) = 0, f(1) = 1
+
+function fibonacci(n){
+    if ( n===0 ) return 0;
+    if ( n===1 ) return 1;
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+console.log(fibonacci(5));
