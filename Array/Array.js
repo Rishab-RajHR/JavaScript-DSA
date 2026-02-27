b3c514e32e22a43d126db649ac5c21830e926262
Rishab Raj <rr0948555@gmail.com>
2026-02-27 22:40:28 +0530

    Array

--- Array/Array.js
+++ Array/Array.js
@@ -2,6 +2,11 @@
 
 let arr = [10,20,30,40];
 
-console.log(arr[2]);
-arr[1] = 25;
-console.log(arr[1]);
+// console.log(arr[2]);
+// arr[1] = 25;
+// console.log(arr[1]);
+
+for(let i=0; i<arr.length; i++)
+{
+   console.log(arr[i]);
+}
