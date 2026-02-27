14adbb711ed4a669ce32bbf0bb856059f97b97e2
Rishab Raj <rr0948555@gmail.com>
2026-02-27 22:53:04 +0530

    Reverse Array

---
+++ Array/Reverse2.js
@@ -0,0 +1,15 @@
+// Reverse an Array
+
+let arr = [1,2,3,4];
+let start = 0;
+let end = arr.length-1;
+
+while(start < end){
+    let temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    start++;
+    end--;
+}
+console.log(arr);
