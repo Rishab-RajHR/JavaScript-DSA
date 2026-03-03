2041dccf610f39e564ae1584d57cac5c8b176795
Rishab Raj <rr0948555@gmail.com>
2026-03-03 11:08:05 +0530

    Freq

--- String/FreqCount.js
+++ String/FreqCount.js
@@ -2,6 +2,8 @@
 
 function charFreq(str) {
     let freq = {};
+    // For handling the Uppercase value
+    str = str.toLowerCase().replace(/\s+/g,"");
 
     for(let ch of str){
         freq[ch] = (freq[ch] || 0)+1;
