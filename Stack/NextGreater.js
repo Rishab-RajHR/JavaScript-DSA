760b53b731eb5f3b8263fe5e2fdc06afb31ca301
Rishab Raj <rr0948555@gmail.com>
2026-03-08 14:36:30 +0530

    Next Greater

---
+++ Stack/NextGreater.js
@@ -0,0 +1,23 @@
+// Next Greater Element
+
+function nextGreEle(arr) {
+    let n = arr.length;
+    let res = new Array(n).fill(-1);
+    let stack = [];
+
+    for(let i=n-1; i>=0; i--) {
+        // POP smaller elements
+        while(stack.length > 0 && stack[stack.length-1] <= arr[i]){
+            stack.pop();
+        }
+        // Top of the stack is next greater (if exist)
+        if(stack.length>0){
+            res[i] = stack[stack.length-1];
+        }
+
+        // Push current element
+        stack.push(arr[i]);
+    }
+    return res;
+}
+console.log(nextGreEle([4,5,2,10,8]));
