b4d18f1b9b7c39ca646c916c38946f390a159b1f
Rishab Raj <rr0948555@gmail.com>
2026-08-18 22:09:00 +0530

    Quick Sort

---
+++ Sort/quickSort.js
@@ -0,0 +1,23 @@
+function quickSort(arr) {
+  // Base Case
+  if (arr.length <= 1) {
+      return arr;
+  }
+  let pivot = arr[arr.length - 1];
+  let left = [];
+  let right = [];
+
+   for(let i=0; i<arr.length; i++){
+      if(arr[i] < pivot) {
+         left.push(arr[i]);
+      } else {
+          right.push(arr[i]);
+      }
+   }
+   return [
+      ...quickSort(left),
+      pivot,
+      ...quickSort(right)
+   ]
+}
+console.log(quickSort([8, 3, 1, 7, 0, 10, 2]));
