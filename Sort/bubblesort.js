76e4f9656fe7376daf1851b98a927b3cddb209ff
Rishab Raj <rr0948555@gmail.com>
2026-08-18 16:30:59 +0530

    Bubble Sort

---
+++ Sort/bubblesort.js
@@ -0,0 +1,17 @@
+// Bubble Sort
+
+function bubbleSort(arr){
+    let n = arr.length;
+
+    for(let i=0; i<n; i++) {
+       for(let j=0; j<n; j++) {
+          if(arr[j] > arr[j+1]){
+              let temp = arr[j];
+              arr[j] = arr[j+1];
+              arr[j+1] = temp;
+          }
+       }
+    }
+    return arr;
+}
+console.log(bubbleSort([5,3,8,4,2]));
