4a3f5ded9eff41ffdb51d3dcc34e279ab5deff11
Rishab Raj <rr0948555@gmail.com>
2026-08-19 15:01:17 +0530

    binary Tree

---
+++ Tree/binary.js
@@ -0,0 +1,17 @@
+class TreeNode{
+   constructor(value){
+     this.value = value;
+     this.left = null;
+     this.right = null;
+   }
+}
+
+let root = new TreeNode(10);
+
+root.left = new TreeNode(20);
+root.right = new TreeNode(30);
+
+root.left.left = new TreeNode(40);
+root.left.right = new TreeNode(50);
+
+console.log(root);
